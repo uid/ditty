c590e6cde1863a611f30c35949081c775c6dd74c
Tom Lieber <tom@alltom.com>
2012-02-02 20:11:28 -0500

    script for importing from the legacy patterns JSON to the database

---
+++ lib/import_from_json.rb
@@ -0,0 +1,18 @@
+require File.join(".", File.dirname(__FILE__), "..", "config", "environment.rb")
+
+json_patterns = ActiveSupport::JSON.decode(File.read(File.join(Rails.root, "app", "views", "default", "patterns.json.erb")))
+
+Pattern.transaction do
+  json_patterns.values.each do |json_pattern|
+    Pattern.create! do |pattern|
+      pattern.show = json_pattern["show"].nil? ? true : json_pattern["show"]
+      pattern.representations = JSON.pretty_generate(json_pattern["representations"])
+      pattern.arguments = JSON.pretty_generate(json_pattern["arguments"] || [])
+      if Hash === json_pattern["meaning"]
+        pattern.meaning = JSON.pretty_generate([json_pattern["meaning"]])
+      else
+        pattern.meaning = JSON.pretty_generate(json_pattern["meaning"])
+      end
+    end
+  end
+end
