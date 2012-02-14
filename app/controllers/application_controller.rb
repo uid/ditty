a2f882c13467c51b8cbec94f06f73f34b1fa4b89
Tom Lieber <tom@alltom.com>
2012-02-13 21:21:42 -0500

    redirect people to playditty.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :ensure_domain
+  
+  def ensure_domain
+    return if Rails.env != "production"
+    
+    canonical_domain = "playditty.com"
+    if request.host != canonical_domain
+      redirect_to(request.protocol + canonical_domain + request.fullpath)
+    end
+  end
 end
