a9b18bc824891b5ce6af0aeaf6d40bd4b971ead7
Tom Lieber <tom@alltom.com>
2012-02-14 12:00:53 -0500

    delete a user's patterns when you delete the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :patterns, :foreign_key => "creator_id", :dependent => :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
