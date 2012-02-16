10602fc112f74016fbe4ba5c52d3163b75b70740
Tom Lieber <tom@alltom.com>
2012-02-15 20:53:34 -0500

    added a new helper

--- app/assets/javascripts/canvas/random.js
+++ app/assets/javascripts/canvas/random.js
@@ -79,3 +79,16 @@ function cpsMap(f, c, e, arr) {
   }
   loop(0)
 }
+
+// returns an array with the provided elements removed
+// (the provided elements are unnamed arguments after 'arr')
+function arrayRemove(arr) {
+    var what, a = arguments, L = a.length, ax
+    while(L > 1 && arr.length) {
+        what= a[--L]
+        while((ax = arr.indexOf(what))!= -1) {
+            arr.splice(ax, 1)
+        }
+    }
+    return arr
+}
