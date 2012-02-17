ad9ae79cdb7e06eec6b098b3e10a68f26b6286d4
Tom Lieber <tom@alltom.com>
2012-02-17 10:06:59 -0500

    associate new patterns with the current user

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -184,7 +184,7 @@ function environmentLoaded() {
   
   var newBubble = function(pattern) {
     var saveAfter = !pattern
-    pattern = pattern || new Pattern({ representations: [new Template("My Code")] })
+    pattern = pattern || new Pattern({ creator: { id: currentUser.id }, representations: [new Template("My Code")] })
     var myCode = new PatternView(pattern)
     codeCanvas.accept(myCode, true /* propagate */)
     myCode.dom.css({ position: "absolute", left: "10px", top: "10px" })
