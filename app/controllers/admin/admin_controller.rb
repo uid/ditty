4b81ce159ed2e9a5c825e39fbfbccd617596acc3
Tom Lieber <tom@alltom.com>
2012-02-21 17:40:16 -0500

    require admin password for the entire site

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,17 +1,3 @@
 class Admin::AdminController < ApplicationController
   layout "admin"
-  
-  before_filter :verify_access
-  
-  def verify_access
-    return if Rails.env != "production"
-    
-    authenticate_or_request_with_http_basic("App Realm") do |username, password|
-      if ENV["ADMIN_USER"] && ENV["ADMIN_PASSWORD"]
-        ENV["ADMIN_USER"] == username && ENV["ADMIN_PASSWORD"] == password
-      else
-        false
-      end
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :ensure_domain
+  before_filter :verify_access
   
   def ensure_domain
     return if Rails.env != "production"
@@ -9,6 +10,18 @@ class ApplicationController < ActionController::Base
     canonical_domain = "playditty.com"
     if request.host != canonical_domain
       redirect_to(request.protocol + canonical_domain + request.fullpath)
+    end
+  end
+  
+  def verify_access
+    return if Rails.env != "production"
+    
+    authenticate_or_request_with_http_basic("App Realm") do |username, password|
+      if ENV["ADMIN_USER"] && ENV["ADMIN_PASSWORD"]
+        ENV["ADMIN_USER"] == username && ENV["ADMIN_PASSWORD"] == password
+      else
+        false
+      end
     end
   end
 end
