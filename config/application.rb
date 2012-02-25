64b05b7537ce7d3a0a77a2ca0c80e45a3d504b77
Tom Lieber <tom@alltom.com>
2012-02-24 19:17:49 -0500

    compile audio assets

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,9 @@ module Ditty
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # also compile audio assets
+    config.assets.paths << Rails.root.join("app", "assets", "audio").to_s
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
