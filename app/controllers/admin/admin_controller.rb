eb6957a5bdbab5b3717e98de87371b6a9a63355e
Tom Lieber <tom@alltom.com>
2012-03-10 12:16:24 -0500

    require admin password for admin area, less secure password for other areas

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,5 @@
 class Admin::AdminController < ApplicationController
+  before_filter :verify_admin_access
+  
   layout "admin"
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :ensure_domain
-  before_filter :verify_access
+  before_filter :verify_user_access
   
   def ensure_domain
     return if Rails.env != "production"
@@ -13,15 +13,39 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def verify_access
-    return if Rails.env != "production"
+  def verify_admin_access
+    verify_access :admin_credentials?
+  end
+  
+  def verify_user_access
+    verify_access :user_credentials?
+  end
+  
+  protected
     
-    authenticate_or_request_with_http_basic("App Realm") do |username, password|
-      if ENV["ADMIN_USER"] && ENV["ADMIN_PASSWORD"]
-        ENV["ADMIN_USER"] == username && ENV["ADMIN_PASSWORD"] == password
+    def user_credentials? username, password
+      admin_credentials?(username, password) || equal_credentials?(ENV["GENERAL_USER"], ENV["GENERAL_PASSWORD"], username, password)
+    end
+    
+    def admin_credentials? username, password
+      equal_credentials? ENV["ADMIN_USER"], ENV["ADMIN_PASSWORD"], username, password
+    end
+    
+    # checks that real_username and real_password are not blank, and equal the given credentials
+    def equal_credentials? real_username, real_password, given_username, given_password
+      if !real_username.blank? && !real_password.blank?
+        real_username == given_username && real_password == given_password
       else
         false
       end
     end
-  end
+    
+    # credentials_method: a method taking a username and password, returning true or false
+    def verify_access credentials_method
+      return if Rails.env != "production"
+      
+      authenticate_or_request_with_http_basic("App Realm") do |username, password|
+        send(credentials_method, username, password)
+      end
+    end
 end
