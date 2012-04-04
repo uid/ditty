79d182ab0ac3815860c9ca7a17557c630fc71dbe
Tom Lieber <tom@alltom.com>
2012-04-04 17:29:32 -0400

    notes from the latest status report

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -32,6 +32,26 @@ Tasks.add({
   description: "Output all of the lines of text containing a certain word or phrase."
 })
 
+/*
+do as many of these as you can in an hour
+visible tiers of difficulty
+notes fields
+current input position
+no I/O
+(a) output all of the lines of text containing a certain word or phrase
+(b) replace all instances of a given string with another string
+(c) count the number of lines and words in a file
+(d) count the number of times each word appears in the file
+(e) trim all whitespace from the beginning and end of each line and, reduce multiple consecutive whitespace characters elsewhere to just one
+(f) check that a file contains correctly nested parentheses
+(g) output the sum of all numbers found on each line of text
+(h) print a particular field of a CSV (comma-separated values) file
+(i) remove C and C++ style comments from a file (single line and block)
+(j) pretty-print a simple JSON file
+(k) convert JSON to XML according to some simple rules
+*/
+
+
 
 $(function() {
   $("#loading").hide()
