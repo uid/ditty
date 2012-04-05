2713b0fcebcffcba8a2bc6cad8668447b6236e45
Tom Lieber <tom@alltom.com>
2012-04-05 03:12:40 -0400

    make the secret token secret once again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ditty::Application.config.secret_token = '3ebc4dc479dc2075d3d198df96386635a2ab5673557b9e4556790a4383ef0faad03dc63a505e36b5ac50944997122ceba5da67f6577128af5df0546596a5cd80'
+Ditty::Application.config.secret_token = ENV["SECRET_TOKEN"]
