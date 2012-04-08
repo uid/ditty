a6007ab87c1f4b3a53ef0f491ab724ea98d98e61
Tom Lieber <tom@alltom.com>
2012-04-08 12:34:26 -0400

    removing dead code

--- app/assets/javascripts/canvas/model/nest-collection.js
+++ app/assets/javascripts/canvas/model/nest-collection.js
@@ -4,9 +4,6 @@
 // if the attribute's array has insertions or deletions, those changes won't be reflected in the collection,
 //   though changes to existing items will
 Backbone.Model.prototype.nestCollection = function(attributeName, nestedCollection) {
-  // for(var i = 0; i < nestedCollection.length; i++) {
-  //   this.attributes[attributeName][i] = nestedCollection.at(i).attributes
-  // }
   var arr = this.get(attributeName)
   for(var i = 0; i < nestedCollection.length; i++) {
     arr[i] = nestedCollection.at(i).attributes
