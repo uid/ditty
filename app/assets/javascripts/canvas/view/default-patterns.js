55ed206df8ffd426560223c949dba6b14f98ddc2
Tom Lieber <tom@alltom.com>
2012-04-08 01:59:02 -0400

    forgotten xxx

--- app/assets/javascripts/canvas/view/default-patterns.js
+++ app/assets/javascripts/canvas/view/default-patterns.js
@@ -25,7 +25,7 @@ function addDefaultPatterns() {
   $("#palette").append($("<button>Create New Command +</button>").click(function() {
     var pattern = new Pattern({ representations: [{ template: randomPhrase() }], native_meaning: [] })
     Patterns.add(pattern)
-    var invocation = new Invocation({ pattern: pattern.cid })
+    var invocation = new Invocation({ pattern: pattern.cid }) // XXX: cid
     var view = new View.InvocationView(invocation)
     Globals.canvas.dropped(view)
     view.toggleSource()
