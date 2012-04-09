59fedee777aa5368a38174269359407aea893b1a
Tom Lieber <tom@alltom.com>
2012-04-09 18:41:07 -0400

    recent activity works for stuff that was created or modified

--- app/assets/javascripts/canvas/view/activity.js
+++ app/assets/javascripts/canvas/view/activity.js
@@ -10,6 +10,98 @@ View.ActivityViewer = my.Class({
   },
   
   opened: function() {
+    this._fetch()
+  },
+  
+  _fetch: function() {
+    if(this.loading) {
+      return
+    }
+    
+    this.loading = true
+    
     this.dom.html("<img src='/assets/spinner.gif' />")
+    
+    var error = function() {
+      this.dom.text("Failed to load recent activity.")
+      this.loading = false
+    }
+    
+    var loadEvents = function() {
+      $.ajax({
+        url: "/chat",
+        dataType: "json",
+        success: function(data) {
+          this.dom.empty()
+          var events = data["events"]
+          for(var i in events) {
+            this._addEvent(events[i])
+          }
+          this.loading = false
+        }.bind(this),
+        error: error.bind(this)
+      })
+    }
+    
+    Patterns.fetch({
+      success: loadEvents.bind(this),
+      error: error.bind(this)
+    })
+  },
+  
+  _addEvent: function(ev) {
+    if(ev.type == "pattern_created") this._newPatternDom(ev)
+    else if(ev.type == "pattern_updated") this._updatedPatternDom(ev)
+    else console.log("unrecognized event", ev)
+  },
+  
+  _newPatternDom: function(ev) {
+    var pattern = Patterns.get(ev.pattern.id)
+    if(!pattern) {
+      console.log("activity log mentioned unknown pattern", ev.pattern)
+      return
+    }
+    
+    var pv = new View.BubbleBlower(function(parent) { return new View.InvocationView(new Invocation({ pattern: pattern.id }), { parent: parent }) })
+    
+    var dom = $("<div class='event'></div>").appendTo(this.dom)
+    
+    if(ev.user.id == Globals.currentUser.id) {
+      dom.append($("<span class='user you'></span>").text("You"))
+    } else {
+      dom.append($("<span class='user'></span>").text(ev.user.readable_name))
+    }
+    dom.append(" <span class='action'>created</span> ")
+    dom.append("this " + ev.ago + " ago:")
+    dom.append("<br />")
+    dom.append(pv.dom)
+    
+    // var view = new View.BubbleBlower(function(parent) { return new View.InvocationView(new Invocation({ pattern: pattern.id }), { parent: parent }) })
+    // try {
+    //   var pattern = jsonUnserialize(ev.pattern)
+    //   if(!this.batching) {
+    //     patterns[pattern.id] = pattern
+    //   }
+    //   var pv = new PatternView(pattern)
+    //   dom.append(pv.dom)
+    // } catch(e) {
+    //   dom.append("[error]")
+    // }
+  },
+  
+  _updatedPatternDom: function(ev, dom) {
+    dom.append(ev.ago + " ago, ")
+    dom.append($("<span class='chat-user'></span>").text(ev.user.readable_name))
+    dom.append(" changed ")
+    // try {
+    //   var pattern = jsonUnserialize(ev.pattern)
+    //   if(!this.batching) {
+    //     patterns[pattern.id] = pattern
+    //   }
+    //   var pv = new PatternView(pattern)
+    //   dom.append(pv.dom)
+    // } catch(e) {
+    //   dom.append("[error]")
+    // }
   },
 })
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event
   include Curator::Model
+  include ActionView::Helpers::DateHelper # because I don't give a crap right now
   
   attr_accessor :type # :pattern_created, :pattern_updated, :chat
   attr_accessor :time
@@ -7,6 +8,14 @@ class Event
   
   attr_accessor :chat # if type = :chat
   attr_accessor :pattern # if type = :pattern_created or :pattern_updated
+  
+  def as_json options={}
+    super.merge(ago: ago)
+  end
+  
+  def ago
+    time_ago_in_words(time, true)
+  end
   
   def self.pattern_created pattern
     Event.new type: :pattern_created, time: pattern.created_at, user: pattern.creator, pattern: pattern
