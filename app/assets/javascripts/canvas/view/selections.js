1d2ea2d5b68ade860c3cb32586052883d6196df2
Tom Lieber <tom@alltom.com>
2012-05-02 10:21:59 -0400

    selection handler selects elements according to some heuristics

--- app/assets/javascripts/canvas/view/selections.js
+++ app/assets/javascripts/canvas/view/selections.js
@@ -101,23 +101,61 @@ var SelectionManager = my.Class({
 
 var BubbleSelectionHandler = my.Class({
   selectionBegan: function(e) {
-    this.selected = []
+    this.selectionChanged(e)
   },
   
   selectionChanged: function(e) {
     _.each(this.selected, function(obj) { obj.dom.css({ border: "" }) })
     
-    this.selected = []
+    this.selectionsFromOutside = []
+    this.selections = []
     
     $(".bubble").each(function(i, dom) {
       dom = $(dom)
-      if(e.rect.intersectsDom(dom)) {
-        var obj = View.objFor(dom)
-        if(obj) {
-          this.selected.push(obj)
+      var obj = View.objFor(dom)
+      if(obj) {
+        if(e.rect.intersectsDom(dom)) {
+          this.selections.push(obj)
+        }
+        if(e.rect.intersectsDomFromOutside(dom)) {
+          this.selectionsFromOutside.push(obj)
         }
       }
     }.bind(this))
+    
+    if(this.selectionsFromOutside.length > 0) {
+      // create hashtable mapping to selected objects from their parents
+      var ht = new Hashtable()
+      for(var i in this.selectionsFromOutside) {
+        var obj = this.selectionsFromOutside[i]
+        var parent = obj.directContainer()
+        if(!parent) continue
+        if(ht.containsKey(parent)) {
+          ht.get(parent).push(obj)
+        } else {
+          ht.put(parent, [obj])
+        }
+      }
+      
+      var parentDepth = function(entry) { return entry[0].dom.parents().length }
+      
+      // if multiple are selected with the same parent, select the highest set
+      var groups = _.filter(ht.entries(), function(e) { return e[1].length > 1 })
+      if(groups.length > 0) {
+        // pick the highest group
+        this.selected = _.sortBy(groups, parentDepth)[0][1]
+      } else {
+        // otherwise, take the deepest
+        this.selected = _.sortBy(ht.entries(), parentDepth)[0][1]
+      }
+      
+      // TODO: ensure contiguous
+    } else if(this.selections.length > 0) {
+      // choose the deepest item
+      this.selected = [_.sortBy(this.selections, function(o) { return -o.dom.parents().length })[0]]
+    } else {
+      this.selected = []
+    }
     
     _.each(this.selected, function(obj) { obj.dom.css({ border: "1px solid black" }) })
   },
